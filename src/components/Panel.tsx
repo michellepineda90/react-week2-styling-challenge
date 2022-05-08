4cf19cfc082aec5e29afff4ae0fd1022ee6b9af1
Michele Pineda <michelle.pineda.rivas@gmail.com>
2022-05-07 23:21:28 -0600

    chore: add HTML and CSS for components UI

--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -3,77 +3,78 @@ import './Panel.css';
 
 function Panel() {
     return (
-        <div className='rectangleSix'>
-            <div className=' nameSection'>
+        <div className='panel'>
+            <div className='nameSection'>
                 <div className='btns'>
-                    <input type='button' className='btn panel' />
-                    <input type='button' className='btn panel' />
+                    <input type='button' className='btn btnPanel' />
+                    <input type='button' className='btn btnPanel' />
                 </div>
-                <p className='regularTxt'>Name</p>
-                <div className='profilePic'></div>
+                <div className='name'>
+                    <p className='regularTxt'>Name</p>
+                    <div className='profilePic'></div>
+                </div>
             </div>
             <hr className='lineTwo'></hr>
             <div className='storageSection'>
-                <p className='headerTitle'>Storage</p>
-                <div className='chart'>
-                    <p className='detailFont'>
-                        GB of used
-                    </p>
+                <p className='headerTitle dark'>Storage</p>
+                <div className='chartSection'>
+                    <div className='chart'></div>
+                    <p className='detailFont'>GB of used</p>
                 </div>
                 <div className='storage'>
                     <div className='storageDetail'>
                         <div className='medSquare orange'></div>
                         <div className='text'>
-                            <p className='smallFont'>Documents</p>
-                            <p className='detailFont'>files</p>
+                            <p className='smallFont dark'>Documents</p>
+                            <p className='detailFont file'>files</p>
                         </div>
                         <div className='gbDetail'>
                             <p className='detailFont dark'>GB</p>
                         </div>
                     </div>
-                    <hr />
+                    <hr className='lineThree' />
                     <div className='storageDetail'>
                         <div className='medSquare blue'></div>
                         <div className='text'>
-                            <p className='smallFont'>Documents</p>
-                            <p className='detailFont'>files</p>
+                            <p className='smallFont dark'>Documents</p>
+                            <p className='detailFont file'>files</p>
                         </div>
                         <div className='gbDetail'>
                             <p className='detailFont dark'>GB</p>
                         </div>
                     </div>
-                    <hr />
+                    <hr className='lineThree' />
                     <div className='storageDetail'>
                         <div className='medSquare aqua'></div>
                         <div className='text'>
-                            <p className='smallFont'>Documents</p>
-                            <p className='detailFont'>files</p>
+                            <p className='smallFont dark'>Documents</p>
+                            <p className='detailFont file'>files</p>
                         </div>
                         <div className='gbDetail'>
                             <p className='detailFont dark'>GB</p>
                         </div>
                     </div>
-                    <hr />
+                    <hr className='lineThree' />
                     <div className='storageDetail'>
                         <div className='medSquare gray'></div>
                         <div className='text'>
-                            <p className='smallFont'>Documents</p>
-                            <p className='detailFont'>files</p>
+                            <p className='smallFont dark'>Documents</p>
+                            <p className='detailFont file'>files</p>
                         </div>
                         <div className='gbDetail'>
                             <p className='detailFont dark'>GB</p>
                         </div>
                     </div>
-                    <hr />
+                    <hr className='lineThree' />
                 </div>
-                <div className='buyMoreSpaceSection'>
-                    <div className='colorGradientTwo'></div>
-                    <div className='text'>
-                        <p className='regularTxt'>Buy more space now!</p>
-                            <p className='detailFont'>Upgrade to cloud premium</p>
-                    </div>
-                    <div className='orangeBtn'>Upgrade Account!</div>
+            </div>
+            <div className='buyMoreSpaceSection'>
+                <div className='colorGradientTwo'></div>
+                <div className='text'>
+                    <p className='regularTxt'>Buy more space now!</p>
+                    <p className='detailFont premium'>Upgrade to cloud premium</p>
                 </div>
+                <div className='orangeBtn'>Upgrade Account!</div>
             </div>
         </div>
     );
