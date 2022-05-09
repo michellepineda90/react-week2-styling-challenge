07a3d56da416f665403a20afda10011bf000f2c5
Michele Pineda <michelle.pineda.rivas@gmail.com>
2022-05-08 19:51:43 -0600

    chore: add HTML and CSS for components UI

--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -1,5 +1,43 @@
 import React from 'react';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
 import './Panel.css';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+
+export const data = {
+    datasets: [
+                { 
+                    id: 1,
+                    label: 'Percentage of storage used',
+                    data: [75, 200, 50, 125],
+                    backgroundColor: [
+                    '#4AC29D',
+                    '#FF9F00',
+                    '#C4C4C4',
+                    '#689FF8'
+                    ],
+                    borderRadius: 45,
+                    borderColor: '#00000000',
+                    spacing: 10,
+                    cutout: 52,
+                },
+            ],
+    // TODO: use options object from library to render detail
+    // options: {
+    //     elements: {
+    //         center: {
+    //             text: '85% used',
+    //             color: '#FF6384',
+    //             fontStyle: 'Roboto',
+    //             sidePadding: 20,
+    //             minFontSize: 25,
+    //             lineHeight: 25,
+    //         }
+    //     }
+    // }
+};
 
 function Panel() {
     return (
@@ -18,7 +56,13 @@ function Panel() {
             <div className='storageSection'>
                 <p className='headerTitle dark'>Storage</p>
                 <div className='chartSection'>
-                    <div className='chart'></div>
+                    <div className='chart'>
+                        <Doughnut datasetIdKey='id' data={data} />
+                        <div className='percentageSection'>
+                            <p className='percentageFont'>85%</p>
+                            <p className='subHeader'>Used</p>
+                        </div>
+                    </div>
                     <p className='detailFont'>GB of used</p>
                 </div>
                 <div className='storage'>
